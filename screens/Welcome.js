c803c9374c47619a56d869b1cc72c6c9246c508e
Boban Jaric <jaricboban@yahoo.com>
2022-07-30 14:45:43 +0200

    Last commit for matser

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -10,6 +10,7 @@ import {
   TouchableOpacity,
   Button,
   TextInput,
+  Pressable
 } from "react-native";
 import Carousel, { Pagination } from "react-native-snap-carousel";
 
@@ -33,7 +34,43 @@ const Welcome = () => {
 
   return (
     <>
-      <View>
+      <View style={styles.carouselContainer}>
+        <Carousel
+          data={img}
+          renderItem={({ item, index }) => (
+            <Image
+              key={index}
+              source={item.image}
+            />
+          )}
+          layout='tinder'
+          layoutCardOffset={9}
+          sliderWidth={width}
+          itemWidth={width}
+          sliderHeight={height}
+          autoplay={true}
+          autoplayInterval={3000}
+          bounces={false}
+          loop={true}
+
+        />
+        <Text style={styles.title}>ZLATIBOR ZOOM</Text>
+        <View style={styles.buttonHolder} >
+          <Pressable
+            onPress={() => {
+              alert("Click");
+            }}
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}>
+              Uđi u aplikaciju!
+            </Text>
+          </Pressable>
+        </View>
+      </View>
+
+
+      {/*  <View>
         <ImageBackground
           source={{
             uri: "https://zlatibor-booking.com/staro_selo1.1792ea88ceb94bd8ac32.jpeg",
@@ -64,7 +101,7 @@ const Welcome = () => {
             }
           />
         </ImageBackground>
-      </View>
+      </View> */}
 
       {/* <View style={{ flex: 1, backgroundColor: "black", alignItems: "center" }}>
         <View>
@@ -125,7 +162,7 @@ const Welcome = () => {
   );
 };
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   img: {
     height: height,
     width: width,
@@ -138,6 +175,53 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     padding: 10,
   },
+}); */
+
+const styles = StyleSheet.create({
+  carouselContainer: {
+    height: "100%",
+    width: "100%",
+    backgroundColor: '#fff'
+  },
+  backgroundImage: {
+    height: '100%',
+    width: width
+  },
+  title: {
+    position: 'absolute',
+    top: 200,
+    alignSelf: "center",
+    fontWeight: 'bold',
+    color: "white",
+    fontSize: 45,
+    zIndex:2,
+  },
+  buttonHolder: {
+    position: 'absolute',
+    bottom: 200,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    width: width * 0.7,
+    borderRadius: 10,
+    zIndex: 2,
+    backgroundColor: '#00ffff',
+    padding: 5
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 5,
+    paddingHorizontal: 2,
+  },
+  buttonText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+    textTransform: 'uppercase'
+  }
+
 });
 
 export default Welcome;
