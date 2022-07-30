d13bb222a366da82a88c90cb88d57896eaf722e9
kowcicuros70@gmail.com <kowcicuros70@gmail.com>
2022-07-30 13:09:17 +0200

    test

---
+++ screens/TestGrana.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const TestGrana = () => {
+  return (
+    <View>
+      <Text>TestGrana</Text>
+    </View>
+  );
+};
+
+export default TestGrana;
+
+const styles = StyleSheet.create({});
