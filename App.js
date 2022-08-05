681e89147975a52ea4cb6dd3a3906e793cc252cc
Boban Jaric <jaricboban@yahoo.com>
2022-08-05 17:41:15 +0200

    Folder structured in src

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // custom screens
-import Home from './views/home'
+import Home from './src/views/home'
 
 export default function App() {
   return (
--- firebase.js
+++ firebase.js
@@ -11,6 +11,17 @@ const firebaseConfig = {
     appId: "1:952482744952:web:61ca8fbd959194ff067b8d"
 };
 
+/* const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+};
+
+ */
+
 
 firebase.initializeApp(firebaseConfig);
 
